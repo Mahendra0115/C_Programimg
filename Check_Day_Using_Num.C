d7277b86564f6bc5cd35c547ac760c8c851388b5
Mahendra Singh <126326125+Mahendra0115@users.noreply.github.com>
2024-01-08 22:15:26 +0530

    Add files via upload

---
+++ Check_Day_Using_Num.C
@@ -0,0 +1,66 @@
+#include<stdio.h>
+  void main(){
+    int day;
+
+printf("enter the day number: ");
+scanf("%d",&day);
+
+ switch(day){
+        case 1:
+        printf("sunday");
+        break;
+         case 2:
+        printf("moday");
+        break;
+         case 3:
+       printf("tuesday");
+        break;
+         case 4:
+       printf("wednesday");
+        break;
+         case 5:
+       printf("thirsday");
+        break;
+         case 6:
+        printf("friday");
+        break;
+         case 7:
+       printf("saturday");
+        break;
+        
+         default:
+        printf("saturday");
+        break;
+    }
+    }
+
+
+  
+        
+    //     if(a == 1){
+    //         System.out.println("sunday");
+    //     }
+    //      else if(a == 2){
+    //         System.out.println("monday");
+    //     } 
+    //      else if(a == 3){
+    //         System.out.println("tuesday");
+    //     } 
+    //      else if(a == 4){
+    //         System.out.println("wednesday");
+    //     } 
+    //      else if(a == 5){
+    //         System.out.println("thursday");
+    //     } 
+    //     else if(a == 6){
+    //         System.out.println("friday");
+    //     } 
+    //     else if(a == 7){
+    //         System.out.println("saturday");
+    //     } 
+    //   else{
+    //       System.out.println("not valid number");
+    //   }
+    
+   
+
---
+++ Gretest_Two_Num.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf(" enter the value of a:  ");
+    scanf("%d",&a);
+    
+    printf("enter the value of b:");
+    scanf("%d",&b);
+    
+  
+    
+     
+     if(a<b){
+         printf(" A is grater ");
+         
+     }
+     else{
+         printf("B is greater");
+     }
+     return 0;
+}
+
+    
