cd806352a3fdc8d5120527e834231cffe17fdf99
Mahendra Singh <126326125+Mahendra0115@users.noreply.github.com>
2024-01-22 00:27:40 +0530

    Add files via upload

---
+++ find_area.c
@@ -0,0 +1,43 @@
+ // find greater  in three value  -> {  area = pai(3.14) *r^2  }
+  
+#include <stdio.h>
+    //  find area of circle
+    
+int main()
+{
+     float area ;
+     int r;
+     int pai ;
+     
+    printf("enter the value of r:");
+    scanf("%d",&r);
+    
+    area = 3.14*r*r;
+     
+    printf("Area is :%f", area);
+     
+     
+}
+
+//  find area of rectAngle   {  a = width*height }
+
+
+int main()
+{
+     int  a ;
+      int width;
+      int height ;
+     
+     
+    printf("enter the value of width:");
+    scanf("%d",&width);
+    printf("enter the value of  height:");
+    scanf("%d",&height);
+    
+     a = width*height;
+     
+    printf("Area is :%d", a);
+     
+}
+     
+
