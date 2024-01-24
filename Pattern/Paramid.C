74f32f88f029f1433a166f15e0948dfc74b7c5ec
Mahendra Singh <126326125+Mahendra0115@users.noreply.github.com>
2024-01-25 03:40:33 +0530

    Update Paramid.C

---
+++ Pattern/Paramid.C
@@ -0,0 +1,34 @@
+ /*
+    
+    *
+   * *               PYRAMID STAR PATTERN
+  * * *
+ * * * *
+    
+    */ 
+         
+#include <stdio.h>
+ int main(){
+     int n;
+      printf("enter the number:");
+      scanf("%d",&n);
+         
+         for(int i=1; i<=n; i++){
+            for(int j=n; j>i; j--){
+                printf(" ");
+            }
+            for(int k=1; k<=i; k++){
+                printf("* ");
+            }
+            printf("\n");
+         }
+       
+       
+       
+       
+       
+      return 0;
+ }
+
+
+
